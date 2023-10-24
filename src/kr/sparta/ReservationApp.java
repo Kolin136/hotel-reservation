542a141e09d104bde244a6688ddd272694b1d616
Gwon JunHyeok <jh10253267@gmail.com>
2023-10-24 16:55:54 +0900

    testcommit

--- src/kr/sparta/ReservationApp.java
+++ src/kr/sparta/ReservationApp.java
@@ -3,13 +3,11 @@ package kr.sparta;
 public class ReservationApp {
     public static void main(String[] args) {
 
-        /**ex)
-         * while(true) {
-         * switch(number) {
-         *  case 1
-         *  case 2
-         * }
-         */
-        System.out.println("hello");
+        while(true) {
+            switch () {
+                case
+            }
+        }
+
     }
 }
---
+++ src/kr/sparta/handler/ReserveHandler.java
@@ -0,0 +1,24 @@
+package kr.sparta.handler;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 예약과 관련된 기능 수행.
+ * 해야하는 역할 정보를 입력받아서 객체 생성.
+ *
+ */
+public class ReserveHandler {
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public void reserve() {
+        String name = br.readLine();
+        String tel = br.readLine();
+        //Reservation reservation = new Reservation(name, tel);
+    }
+    public void printPage() {
+        //sout 실행할 메소드.
+    }
+
+
+}
