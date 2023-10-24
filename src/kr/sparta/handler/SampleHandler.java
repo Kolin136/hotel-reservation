f71bfdde76da5da07df517a2377800ea994d8f85
jshstar <jshstar97@gmail.com>
2023-10-24 13:08:19 +0900

    test수정

--- src/kr/sparta/handler/SampleHandler.java
+++ src/kr/sparta/handler/SampleHandler.java
@@ -2,7 +2,9 @@ package kr.sparta.handler;public class SampleHandler {
 
     public void test(){
         System.out.println("test1234 ");
-
+        System.out.println("test1234 ");
+        System.out.println("test1234 ");
+        System.out.println("test1234 ");
 
     }
 
