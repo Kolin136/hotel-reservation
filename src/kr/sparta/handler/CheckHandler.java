b951bcc381d43d92174592bdf5427d6a595115fa
Gwon JunHyeok <jh10253267@gmail.com>
2023-10-26 16:35:28 +0900

    체크핸들러 수정.

--- src/kr/sparta/handler/CheckHandler.java
+++ src/kr/sparta/handler/CheckHandler.java
@@ -17,24 +17,32 @@ public class CheckHandler {
     public Reservation getReservation(String reservationId) {
         Reservation reservation = dao.getInquiry(reservationId);
         return reservation;
-
     }
 
     public void printMyReservation(String reservationId) throws IOException {
         Reservation reservation = getReservation(reservationId);
-        System.out.println("예약하신 내역입니다.");
-        System.out.println("예약 번호: "+ reservationId);
-        System.out.println("성함: " + reservation.getCustomerName());
-        System.out.println("연락처: " + reservation.getCustomerPhoneNumber());
-        System.out.println("예약객실: " + reservation.getRoomId());
-        System.out.println("예약일자: " + reservation.getAccommodationDay());
-        System.out.println("1. 뒤로가기  2.취소하기");
+        if (reservation != null) {
+            System.out.println("예약하신 내역입니다.");
+            System.out.println("예약 번호: " + reservationId);
+            System.out.println("성함: " + reservation.getCustomerName());
+            System.out.println("연락처: " + reservation.getCustomerPhoneNumber());
+            System.out.println("예약객실: " + reservation.getRoomId());
+            System.out.println("예약일자: " + reservation.getAccommodationDay());
+            while (true) {
+                System.out.println("1. 뒤로가기  2.취소하기");
 
-        int choice = getNumber();
-        if (choice == 1) {
-            return;
-        } else if (choice == 2) {
-            cancelReservation(reservationId);
+                int choice = getNumber();
+                if (choice == 1) {
+                    break;
+                } else if (choice == 2) {
+                    cancelReservation(reservationId);
+                    break;
+                } else {
+                    System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+                }
+            }
+        } else {
+            System.out.println("예약된 정보가 없습니다.");
         }
     }
 
@@ -48,18 +56,19 @@ public class CheckHandler {
     }
 
     public void cancelReservation(String reservationId) {
-        System.out.println("해당 예약을 취소하시겠습니까?");
-        System.out.println("1.예    2.아니오");
-        int choice = getNumber();
-        if(choice == 1) {
-            Customer customer = dao.reservationRemove(reservationId);
-            System.out.println("취소가 완료되었습니다.");
-        } else if (choice == 2) {
-            return;
-        } else {
-            System.out.println("잘못누르셨습니다. 메인화면으로 돌아갑니다.");
-            return;
+        while (true) {
+            System.out.println("해당 예약을 취소하시겠습니까?");
+            System.out.println("1.예    2.아니오");
+            int choice = getNumber();
+            if (choice == 1) {
+                Customer customer = dao.reservationRemove(reservationId);
+                System.out.println("취소가 완료되었습니다.");
+                break;
+            } else if (choice == 2) {
+                break;
+            } else {
+                System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
+            }
         }
-
     }
 }
