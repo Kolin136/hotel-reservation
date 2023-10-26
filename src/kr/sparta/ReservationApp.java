77c866085959c51deb11e76903c47afadeb4e346
Dongha <wkdehdgk159@gmail.com>
2023-10-26 19:38:16 +0900

    App dayinput exception revised

--- src/kr/sparta/ReservationApp.java
+++ src/kr/sparta/ReservationApp.java
@@ -31,7 +31,7 @@ public class ReservationApp {
 //        AdminHandler adminHandler = new AdminHandler();
         Scanner sc = new Scanner(System.in);
         ReservationDAO dao = new ReservationDAO();
-        int n = dao.inputManagementRoom();
+        int dayLength = dao.inputManagementRoom();
 
         String phoneNumberPattern = "^\\d{3}-\\d{3,4}-\\d{4}$";
         String namePattern = "^[가-힣]{2,5}$";
@@ -44,7 +44,6 @@ public class ReservationApp {
             System.out.println("------------------------------------------------------");
             System.out.println("1. 예약하기");
             System.out.println("2. 예약확인/취소");
-            System.out.println("3. 방 현황 확인");
             String customerName, customerPhoneNumber;
             int day = 0;
             long cash;
@@ -68,8 +67,7 @@ public class ReservationApp {
                     System.out.println("성함을 입력해주세요.");
                     while(true) {
                         System.out.println("------------------------------------------------------");
-                        customerName = sc.next();
-                        sc.nextLine();
+                        customerName = sc.nextLine();
                         if(Pattern.matches(namePattern,customerName)) {
                             break;
                         } else {
@@ -80,7 +78,7 @@ public class ReservationApp {
                     System.out.println("------------------------------------------------------");
                     System.out.println("전화번호를 입력해주세요.(XXX-XXXX-XXXX)");
                     while(true) {
-                        customerPhoneNumber = sc.next();
+                        customerPhoneNumber = sc.nextLine();
                         if(Pattern.matches(phoneNumberPattern,customerPhoneNumber)) {
                             break;
                         } else {
@@ -113,16 +111,25 @@ public class ReservationApp {
                             System.out.println("------------------------------------------------------");
                             System.out.println("올바른 숫자를 입력해주세요");
                         }
+
                         day = sc.nextInt();
                         sc.nextLine();
+
+                        if(day > dayLength || day < LocalDate.now().getDayOfMonth()) {
+                            System.out.println("------------------------------------------------------");
+                            System.out.println("오늘은 " + LocalDate.now().getMonthValue() + "월 " + LocalDate.now().getDayOfMonth() + "일 입니다.");
+                            System.out.println("올바른 날짜를 입력해주세요");
+                            continue;
+                        }
 
                         if (reserveHandler.show(day) == 0) {
                             System.out.println("------------------------------------------------------");
                             System.out.println("예약이 가능한 객실이 없습니다. 다른 날짜를 입력해주세요.");
+                            System.out.println("------------------------------------------------------");
                         } else {
                             reserveHandler.reserve(customerName, customerPhoneNumber, cash, day);
-                            break;
                         }
+                        break;
                     }
 
                     break;
@@ -130,7 +137,7 @@ public class ReservationApp {
                     System.out.println(ANSI_YELLOW + "\"예약확인\"" + ANSI_RESET);
                     System.out.println("------------------------------------------------------");
                     System.out.println("예약번호를 입력해주세요.");
-                    String customerReservationNumber = sc.next();
+                    String customerReservationNumber = sc.nextLine();
                     checkHandler.printMyReservation(customerReservationNumber);
                     break;
                 case 9:
